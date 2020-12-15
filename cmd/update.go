8c2e3d2e94d8f441c92830080530701916018175
nataprawiraf <nataprawiraf@gmail.com>
2020-12-15 10:44:18 +0700

    add first readme finalize basic command

--- cmd/update.go
+++ cmd/update.go
@@ -23,14 +23,9 @@ import (
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
-	Use:   "update",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "update id [--alias alias] \"new_command\"",
+	Short: "update shell command from database.",
+	Long:  "update shell command from database, see syphon update --help to see update command help.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("update called")
 	},
